a9432299e2f199be6b20ea3531d82167ec4ee83b
JoaoptGaino <joaoptgaino@hotmail.com>
2021-08-31 07:38:55 -0300

    changes to files

--- tech-challenge/dist/movie/movie.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Cache } from 'cache-manager';
-import { PrismaService } from 'src/prisma.service';
-export declare class MovieService {
-    private cacheManager;
-    private readonly prisma;
-    constructor(cacheManager: Cache, prisma: PrismaService);
-    findMovie(name: string): Promise<any>;
-}
--- tech-challenge/dist/services/api.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.api = void 0;
-const axios_1 = require("axios");
-exports.api = axios_1.default.create({
-    baseURL: 'http://www.omdbapi.com/',
-});
-//# sourceMappingURL=api.js.map
