8bc361da60748f8225221152e47ed2ee16cc74fe
JoaoptGaino <joaoptgaino@hotmail.com>
2021-08-31 08:03:38 -0300

    heroku config

--- dist/movie/controllers/movie.controller.d.ts
+++
@@ -1,6 +0,0 @@
-import { MovieService } from '../services/movie.service';
-export declare class MovieController {
-    private readonly movieService;
-    constructor(movieService: MovieService);
-    findMovie(name?: string): Promise<any>;
-}
--- dist/movie/movie.controller.d.ts
+++
@@ -1,9 +0,0 @@
-import { Cache } from 'cache-manager';
-import { MovieService } from './movie.service';
-export declare class MovieController {
-    private readonly movieService;
-    private cacheManager;
-    constructor(movieService: MovieService, cacheManager: Cache);
-    findMovie(name?: string): Promise<any>;
-    findStorage(name: string): Promise<unknown>;
-}
